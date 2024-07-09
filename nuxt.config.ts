212ef6fb99bbd26ee946feaba8bcebc2aeb6a0df
“Leo” <leo80103@gmail.comgit config --global user.email leo80103@gmail.com>
2024-07-09 13:45:02 +0800

    新增應用程式用最新的金鑰

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,17 +8,13 @@ export default defineNuxtConfig({
     public: {
       apiBase: '/api',
       firebase:{
-        type: process.env.FIREBASE_TYPE,
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-        private_key: process.env.FIREBASE_PRIVATE_KEY,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        client_id: process.env.FIREBASE_CLIENT_ID,
-        auth_uri: process.env.FIREBASE_AUTH_URI,
-        token_uri: process.env.FIREBASE_TOKEN_URI,
-        auth_provider_X509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+        apiKey: process.env.FIREBASE_APIKEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
         storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID
       }
     }
   },
--- plugins/firebaseInit.client.js
+++ plugins/firebaseInit.client.js
@@ -1,7 +1,9 @@
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
 
 // 定義變量來存儲 Firebase 應用和 Storage 實例
 let firebaseApp = false;
+let analytics
 
 // 函數：初始化並返回 Firebase 應用實例
 export default defineNuxtPlugin(() => {
@@ -15,6 +17,7 @@ export default defineNuxtPlugin(() => {
 
     // 初始化 Firebase 應用
     firebaseApp = initializeApp(firebaseConfig);
+    analytics = getAnalytics(firebaseApp);
     console.log('Plugin: firebase init !', firebaseApp);
   }
 
