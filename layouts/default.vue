d46832e325d4345925a014db79a13f5a6d5e93c0
“Leo” <leo80103@gmail.comgit config --global user.email leo80103@gmail.com>
2024-07-15 16:32:14 +0800

    準備測試應用

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,19 @@
     <div class="grid grid-cols-12 gap-4 h-full">
       <section class="col-span-12 sm:col-span-3 shadow-xl">
         <div class="mt-12 ">
-          <div class="px-3" v-for="{ link, name, descript } in links" :key="link.name">
-            <NuxtLink :to="link">
-              <UTooltip class="w-full" :text="descript" :popper="{ placement: 'right' }">
-                <UButton variant="outline" class="linkBtn transition-colors my-1 py-2 bg-phototaxisBlue-50 hover:bg-phototaxisBlue-100
-                  text-phototaxisBlue-500 w-full rounded-lg">
-                  <p class="text-center w-full">{{ name }}</p>
-                </UButton>
-              </UTooltip>
-            </NuxtLink>
+          <div class="px-3" v-for="{ name, childLinks } in links" :key="links.name">
+            <p class="font-bold">{{ name }}</p>
+            <div v-for="{ link, name, descript } in childLinks" :key="link.name">
+              <NuxtLink :to="link">
+                <UTooltip class="w-full" :text="descript" :popper="{ placement: 'right' }">
+                  <UButton variant="outline" class="linkBtn transition-colors my-1 py-2 bg-phototaxisBlue-50 hover:bg-phototaxisBlue-100
+                    text-phototaxisBlue-500 w-full rounded-lg">
+                    <p class="text-center w-full">{{ name }}</p>
+                  </UButton>
+                </UTooltip>
+              </NuxtLink>
+            </div>
+            <br>
           </div>
         </div>
       </section>
@@ -40,20 +44,45 @@ const methodColor = (color)=>{
 }
 
 const links = ref([
-  {
-    link: '/storage/uploadFile',
-    name: 'uploadFile',
-    descript: '可讓您上傳及分享使用者原創內容 (例如圖片和影片)',
+  { 
+    name: "storage",
+    childLinks: [
+      {
+      link: '/storage/uploadFile',
+      name: 'uploadFile',
+      descript: '可讓您上傳及分享使用者原創內容 (例如圖片和影片)',
+      },
+      {
+        link: '/storage/listFile',
+        name: 'listFile',
+        descript: '可讓您列出 Cloud Storage 值區的內容',
+      }
+    ] 
   },
-  {
-    link: '/storage/listFile',
-    name: 'listFile',
-    descript: '可讓您列出 Cloud Storage 值區的內容',
+  { 
+    name: "firestore",
+    childLinks: [
+      {
+        link: '/firestore/getData',
+        name: 'getData',
+        descript: '取得儲存在 Cloud Firestore 中的資料',
+      },
+      {
+        link: '/firestore/postData',
+        name: 'postData',
+        descript: '新增資料在 Cloud Firestore 中',
+      }
+    ] 
   },
-  {
-    link: '/firestore/getData',
-    name: 'getData',
-    descript: '取得儲存在 Cloud Firestore 中的資料',
+  { 
+    name: "Example 應用測試",
+    childLinks: [
+      {
+        link: '/example/article',
+        name: '文章應用測試',
+        descript: 'Article 文章應用測試',
+      }
+    ] 
   }
 ]);
 
--- pages/example/article.vue
+++ pages/example/article.vue
@@ -2,11 +2,11 @@
   <section class="grid grid-cols-9 gap-4 mt-12">
     <!-- 說明區塊 -->
     <section class="col-span-9">
-      <h1 class="font-bold">Cloud Firestore - getData 取得資料</h1>
+      <h1 class="font-bold">Example - 文章應用測試</h1>
     </section>
     <!-- 操作區塊 -->
     <section class="col-span-9">
-      <UButton @click="getData">執行取的資料</UButton>
+      <UButton @click="postData">執行新增資料</UButton>
       <vue-json-pretty :data="result" />
     </section>
   </section>
@@ -16,26 +16,24 @@
 import { getFirestore, collection, doc, setDoc, getDocs, query } from "firebase/firestore";
 import { getApp } from 'firebase/app';
 
+// 隨機字串產生
+import cryptoRandomString from 'crypto-random-string';
+
 const result = ref(null);
 
 // 引用初始化的 firebase 實例
 let firestore
 
-// 取得資料
-const getData = async () => {
+// 新增資料
+const postData = async () => {
   try {
-    const q = query(collection(firestore, "testPosts"));
-    const querySnapshot = await getDocs(q);
-    console.log(querySnapshot);
-    const data = querySnapshot.docs.map(doc => {
-      
-      return { 
-        id: doc.id,
-        ...doc.data()
-      }
+    const newPost = await setDoc(doc(firestore, "testPosts", cryptoRandomString({length: 20})), {
+      content: "新增測試文章內容03",
+      title: "新增測試文章內容03",
+      image: "新增測試文章圖片03"
     });
-    console.log(data);
-    result.value = data;
+    console.log("新增測試文章成功！");
+    result.value = "新增測試文章成功！";
   } catch (error) {
     console.error(error);
   }
