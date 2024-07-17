3ecd8ad196f0ebefcdbb7d2196d80da56f2e074e
“Leo” <leo80103@gmail.comgit config --global user.email leo80103@gmail.com>
2024-07-17 16:25:50 +0800

    auth 錯誤提示增加

--- composables/auth.js
+++ composables/auth.js
@@ -19,7 +19,19 @@ export const login = async (email, password) => {
     const userCredential = await signInWithEmailAndPassword(initAuth(), email, password);
     return userCredential
   } catch(error) {
-    throw error
+    console.error(error);
+    let errorMessage;
+    switch (error.code) {
+      case 'auth/invalid-email':
+        errorMessage = '電子郵件地址格式不正確。';
+        break;
+      case 'auth/invalid-credential':
+        errorMessage = '帳號或密碼不正確。';
+        break;
+      default:
+        errorMessage = '登入過程中發生錯誤。';
+    }
+    throw new Error(errorMessage);
   }
 };
 
@@ -29,7 +41,9 @@ export const userSignout = async () => {
     const userCredential = await signOut(initAuth());
     return userCredential
   } catch(error) {
-    throw error
+    console.error(error);
+    let errorMessage = '登出過程中發生錯誤。';
+    throw new Error(errorMessage);
   }
 };
 
@@ -45,6 +59,21 @@ export const singup = async (email, password) => {
     const userCredential = await createUserWithEmailAndPassword(initAuth(), email, password);
     return userCredential
   } catch(error) {
-    throw error
+    console.error(error);
+    let errorMessage;
+    switch (error.code) {
+      case 'auth/email-already-in-use':
+        errorMessage = '該電子郵件地址已經被使用。';
+        break;
+      case 'auth/invalid-email':
+        errorMessage = '電子郵件地址格式不正確。';
+        break;
+      case 'auth/weak-password':
+        errorMessage = '密碼強度不足。';
+        break;
+      default:
+        errorMessage = '註冊過程中發生錯誤。';
+    }
+    throw new Error(errorMessage);
   }
 };
